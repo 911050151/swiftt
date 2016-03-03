01790a2c599a11734078ad21544253389e43fe93
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-03-03 11:54:31 +0800

    Initial Commit

---
+++ swiftt/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftt
+//
+//  Created by 20131105805李建青 on 16/3/3.
+//  Copyright © 2016年 20131105805李建青. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

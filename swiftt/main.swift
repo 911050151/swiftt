577fff24094682afb691d3220df92af84b85b711
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-03-07 21:17:41 +0800

    swift1
    简单的程序 加减 赋值

--- swiftt/main.swift
+++ swiftt/main.swift
@@ -13,3 +13,35 @@ b=200
 c=a+b
 print("\(a)+\(b)=\(c)")
 
+/*import Cocoa
+var i:Int
+for i=1;i<10;i++ {
+    print("\(i)")
+}
+*/
+
+
+/*
+import Cocoa
+var i:Int
+i=0
+while i<10 {
+    print("\(i)")
+    i++
+}
+*/
+/*
+import Cocoa
+func hello(num:Int){
+    print("hello!\(num)")
+}
+for var i in 1...10{
+    hello(i)
+}
+*/
+
+import Cocoa
+func add(x:Int,_ y:Int,_ z:Int){
+    print("\(x)+\(y)+\(z)=\(x+y+z)")
+}
+add(10,20,30)
